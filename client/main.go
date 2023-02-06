a806654a88abc9b60ed038994226b4df85a9ebd1
Sreejit-K <kanjarla.sreejit@gmail.com>
2023-02-06 12:08:07 +0530

    Setuped a basic client and created an action event for the greetAPerson Server call

--- client/main.go
+++ client/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/Sreejit-K/GO-gRPC-basic-Project/pb"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const (
+	port = ":9090" // port for the client to listen on
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost"+port, grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	if err != nil {
+		log.Fatal("didnt connect to server %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewGreetingServiceClient(conn)
+
+	// names := &pb.ReqList{
+	// 	Names: []string{"Sreejith", "Alice", "Durgesh"},
+	// }
+
+	callSayHello(client)
 }
