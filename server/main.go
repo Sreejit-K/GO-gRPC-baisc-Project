270a94402fd56ea52dc3b41b0176a81526a692e2
Sreejit-K <kanjarla.sreejit@gmail.com>
2023-02-06 11:26:13 +0530

    Started the server on port 9090

--- server/main.go
+++ server/main.go
@@ -1,5 +1,39 @@
 package main
 
+import (
+	"log"
+	"net"
+
+	"github.com/Sreejit-K/GO-gRPC-basic-Project/pb"
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":9090" // declare the port that you want to operate on / server to run on
+)
+
+type helloServer struct {
+	pb.GreetingServiceServer
+}
+
 func main() {
+
+	// Listener
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("Failed to start the server %v", err)
+	}
+
+	// Now we have the port we need to create the GRPC Server next
+
+	grpcServer := grpc.NewServer()
+
+	// we need to register the service that we have created in the proto file
+	pb.RegisterGreetingServiceServer(grpcServer, &helloServer{})
+	log.Println("Server has started on the port %v", lis.Addr().String())
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Failed to start %v", err)
+	}
 
 }
